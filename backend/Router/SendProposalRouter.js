f8a01015f197cb359f0074fa4f8c38e19945ffa2
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-07-09 13:54:52 +0530

    Update SendProposalRouter.js

--- backend/Router/SendProposalRouter.js
+++ backend/Router/SendProposalRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { sendProposalForSignature, checkIfSigned } = require("../Controller/SendProposalController");
+const { sendProposalForSignature, checkIfSigned, getEnvelopesByProjectId } = require("../Controller/SendProposalController");
 
 const router = express.Router();
 
 router.post('/sendProposalForSignature', sendProposalForSignature);
 router.get('/checkIfSigned/:envelopeId', checkIfSigned);
+router.get('/getEnvelopesByProjectId/:project_id', getEnvelopesByProjectId);
 
 module.exports = router;
